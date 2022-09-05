ccbdfb92fe99868f71e0112bb33c82391c7d323b
JestrTulip <dayanndalmeida@gmail.com>
2022-09-05 18:43:44 -0400

    fixed frac

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -35,7 +35,7 @@ uint64_t fixedpoint_whole_part(Fixedpoint val) {
 uint64_t fixedpoint_frac_part(Fixedpoint val) {
   // TODO: implement
   assert(0);
-  return 0UL;
+  return val.frac;
 }
 
 Fixedpoint fixedpoint_add(Fixedpoint left, Fixedpoint right) {
