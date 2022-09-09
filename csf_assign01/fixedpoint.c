dab81b2fad5a7929943565c2cc65fe992d4f6d63
JestrTulip <dayanndalmeida@gmail.com>
2022-09-08 20:04:13 -0400

    partly implemented create from hex

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <assert.h>
+#include <inttypes.h>
 #include "fixedpoint.h"
 
 // You can remove this once all of the functions are fully implemented
@@ -19,14 +20,37 @@ Fixedpoint fixedpoint_create2(uint64_t whole, uint64_t frac) {
 }
 
 Fixedpoint fixedpoint_create_from_hex(const char *hex) {
-  // TODO: implement
+  uint64_t tag = 1;
+  uint64_t whole = 0;
+  uint64_t frac = 0;
+
+  char w_part[64];
+  char f_part[64];
   char t_str[64]; 
+
   strcpy(t_str, hex);
-  if (strtok(t_str, ".") == NULL) {
-    
+
+  char *ptr = strtok(t_str, ".");
+  if (ptr == NULL) {
+    if (strlen(hex) <= 17) {
+      if (hex[0] == '-') { 
+        tag = 0;
+        whole = hex_to_int(hex); 
+      }
+    }
   }
+  else if (ptr != NULL) { 
+    //check for negative and stuff
+    strcpy(w_part, ptr);
+    whole = hex_to_int(w_part); 
+    ptr = strtok(NULL, "."); 
+    strcpy(f_part, ptr); 
+    frac = hex_to_int(f_part); 
+  }
+  
+  Fixedpoint result = {whole, frac, 1}; 
   assert(0);
-  return DUMMY;
+  return result;
 }
 
 uint64_t fixedpoint_whole_part(Fixedpoint val) {
@@ -132,4 +156,29 @@ char *fixedpoint_format_as_hex(Fixedpoint val) {
   char *s = malloc(20);
   strcpy(s, "<invalid>");
   return s;
+}
+
+uint64_t hex_to_int(const char *hex) {
+  uint64_t decimal = 0, base = 1; 
+  
+  int length = strlen(hex);
+
+  for (int i = length--; i >= 0; i--) {
+
+    if (hex[i] >= '0' && hex[i] <= '9') { 
+      decimal += (hex[i] - 48) * base; 
+      base *= 16; 
+    }
+    else if (hex[i] >= 'A' && hex[i] <= 'F') { 
+      decimal += (hex[i] - 55) * base; 
+      base *= 16; 
+    }
+    else if (hex[i] >= 'a' && hex[i] <= 'f') { 
+      decimal += (hex[i] - 87) * base;
+      base *= 16; 
+    }
+  }
+
+  return decimal;
+
 }
