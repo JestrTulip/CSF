1fd34408121df2a982d7eb3d583b65804501f82f
aetherk13 <kedar.krishnan29@gmail.com>
2022-10-11 18:34:50 -0400

    small error in arguments fixed

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -5,19 +5,21 @@
 using namespace std;
 
 int main(int argc, char **argv){
+    
+    int set_num = (int) *argv[1];
+    int block_num = (int) *argv[2];
+    int block_size = (int) *argv[3];
 
-    if(argc !=7 || !check_power_of_2(argc[1]) || !check_power_of_2(argc[2]) || !check_power_of_2(argc[3]) || argc[3] < 4
-        || !(!strcmp(argv[4], "write_allocate") || !strcmp(arg[4], "no_write_allocate"))
-        || !(!strcmp(argv[5], "write_through") || !strcmp(arg[5], "write_back"))
+    if(argc !=7 || !check_power_of_2(set_num) || !check_power_of_2(block_num) || !check_power_of_2(block_size) || (block_size < 4)
+        || !(!strcmp(argv[4], "write_allocate") || !strcmp(argv[4], "no_write_allocate"))
+        || !(!strcmp(argv[5], "write_through") || !strcmp(argv[5], "write_back"))
         || !(!strcmp(argv[6], "lru") || !strcmp(argv[6], "write fifo"))){
         return 0;
     }
 
-    if(!strcmp(arg[4], "no_write_allocate") && !strcmp(arg[5], "write_back")){
+    if(!strcmp(argv[4], "no_write_allocate") && !strcmp(argv[5], "write_back")){
         return 0;
     }
 
-    int set_num = argv[1];
-    int block_num = argv[2];
-    int block_size = argv[3];
+
 }
