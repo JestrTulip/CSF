38f50127eb04a8382ff98e8db669d0559641ff61
aetherk13 <kedar.krishnan29@gmail.com>
2022-10-13 16:47:07 -0400

    starter functions for cache management added

--- csf_assign03/cache_functions.cpp
+++ csf_assign03/cache_functions.cpp
@@ -7,3 +7,16 @@ int check_power_of_2(int val){
     if(ceil(log2(val))== floor(log2(val))) { return 1;}
 	else { return 0;}
 }
+
+uint32_t get_tag(uint32_t address, uint32_t set_num, uint32_t block_size){
+    return address >> (set_num + block_size);
+}
+
+uint32_t get_index(uint32_t address, uint32_t set_num, uint32_t block_size){
+    address = address << (32 - set_num - block_size);
+    return address >> (32 - set_num);
+}
+
+bool store_to_cache(uint32_t address, uint32_t set_num, uint32_t block_size, bool write_allocate, bool write_through,  bool lru);
+
+void load_to_cache(uint32_t address, uint32_t set_num, uint32_t block_size, bool lru);
--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -1,4 +1,4 @@
-#include "cache_functions.h"
+#include "cache_functions.cpp"
 #include <iostream>
 #include <cstring>
 
@@ -6,6 +6,8 @@ using namespace std;
 
 int main(int argc, char **argv){
     
+    //check if args are integers
+
     int set_num = (int) *argv[1];
     int block_num = (int) *argv[2];
     int block_size = (int) *argv[3];
@@ -13,13 +15,21 @@ int main(int argc, char **argv){
     if(argc !=7 || !check_power_of_2(set_num) || !check_power_of_2(block_num) || !check_power_of_2(block_size) || (block_size < 4)
         || !(!strcmp(argv[4], "write_allocate") || !strcmp(argv[4], "no_write_allocate"))
         || !(!strcmp(argv[5], "write_through") || !strcmp(argv[5], "write_back"))
-        || !(!strcmp(argv[6], "lru") || !strcmp(argv[6], "write fifo"))){
+        || !(!strcmp(argv[6], "lru") || !strcmp(argv[6], "fifo"))){
         return 0;
     }
 
     if(!strcmp(argv[4], "no_write_allocate") && !strcmp(argv[5], "write_back")){
         return 0;
     }
+
+
+    bool write_allocate = !strcmp(argv[4], "write_allocate");
+    bool write_through = !strcmp(argv[5], "write_through");
+    bool lru = !strcmp(argv[6], "lru");
+    
+    new Cache cache;
+
 
 
 }
