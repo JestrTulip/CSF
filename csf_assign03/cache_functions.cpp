77d7823c86fe0d81a79be1b0af460dde3f86124c
aetherk13 <kedar.krishnan29@gmail.com>
2022-10-25 15:55:27 -0400

    actually fixed slot indexing

--- csf_assign03/cache_functions.cpp
+++ csf_assign03/cache_functions.cpp
@@ -85,8 +85,8 @@ std::tuple<uint32_t, uint32_t, uint32_t> store_to_cache(Cache cache, uint32_t ad
 
     incrementTime(cache);
     
-    //can get using Set &s = cache.sets[index];
-
+    //can get set using Set &s = cache.sets[index];
+    
     for (std::vector<Slot>::iterator it = cache.sets[currindex].slots.begin() ; it != cache.sets[currindex].slots.end(); ++it) {
         if(currtag == it->tag && it->valid){
             if(write_through){
@@ -133,28 +133,26 @@ std::tuple<uint32_t, uint32_t, uint32_t> load_to_cache(Cache cache, uint32_t add
 
     incrementTime(cache);
 
-    for (std::vector<Set>::iterator it = cache.sets.begin() ; it != cache.sets.end(); ++it) {
-        for (std::vector<Slot>::iterator it = cache.sets[currindex].slots.begin() ; it != cache.sets[currindex].slots.end(); ++it) {
-            if(currtag == it->tag){
-                it->access_ts = 0;
-                loadHit = 1;
-                cycles += 100 * block_size / 4;
-            }
+    for (std::vector<Slot>::iterator it = cache.sets[currindex].slots.begin() ; it != cache.sets[currindex].slots.end(); ++it) {
+        if(currtag == it->tag){
+            it->access_ts = 0;
+            loadHit = 1;
+            cycles += 100 * block_size / 4;
         }
+    }
 
-        //tag not found so a slot must be evicted
-        if(!loadHit && lru){
-            loadMiss = 1;
-            for (std::vector<Slot>::iterator it = cache.sets[currindex].slots.begin() ; it != cache.sets[currindex].slots.end(); ++it) {
-                if(it->access_ts > maxaccess_ts){
-                    maxaccess_ts = it->access_ts;
-                    evicted = it;
-                }
+    //tag not found so a slot must be evicted
+    if(!loadHit && lru){
+        loadMiss = 1;
+        for (std::vector<Slot>::iterator it = cache.sets[currindex].slots.begin() ; it != cache.sets[currindex].slots.end(); ++it) {
+            if(it->access_ts > maxaccess_ts){
+                maxaccess_ts = it->access_ts;
+                evicted = it;
             }
-            if(!evicted->valid) { cycles += 100 * block_size / 4;} //add to cycles
-            evicted->tag = currtag;
-            evicted->load_ts = 0;
         }
+        if(!evicted->valid) { cycles += 100 * block_size / 4;} //add to cycles
+        evicted->tag = currtag;
+        evicted->load_ts = 0;
     }
     return {loadHit, loadMiss, cycles};
 }
