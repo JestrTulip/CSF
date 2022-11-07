8bda15b05efe38dab94b0e7f415c526ce50e5474
aetherk13 <kedar.krishnan29@gmail.com>
2022-11-07 17:43:44 -0500

    started main function handling

--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -46,6 +46,8 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
   // TODO: implement
   if((end-begin) <= threshold){
     qsort(arr, end-begin+1, sizeof(uint64_t), cmpfunc);
+  } else{
+
   }
 }
 
@@ -63,15 +65,39 @@ int main(int argc, char **argv) {
   if (end != argv[2] + strlen(argv[2]))
     /* TODO: report an error (threshold value is invalid) */;
 
+
   // TODO: open the file
+  int fd = open(filename, O_RDWR);
+
+  if (fd < 0) {
+    // file couldn't be opened: handle error and exit
+  }
+
 
   // TODO: use fstat to determine the size of the file
+  struct stat statbuf;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0) {
+      // handle fstat error and exit
+  }
+  size_t file_size_in_bytes = statbuf.st_size;
+
 
   // TODO: map the file into memory using mmap
+  int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (data == MAP_FAILED) {
+      // handle mmap error and exit
+  }
+
 
   // TODO: sort the data!
+  merge_sort(data, 0, sizeof(data)/sizeof(uint64_t)-1, threshold);
+
 
   // TODO: unmap and close the file
-
+  munmap(data, file_size_in_bytes);
+  int fd = close(filename);
+  
   // TODO: exit with a 0 exit code if sort was successful
+  return 0;
 }
